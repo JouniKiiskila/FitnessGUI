c6408a645f353ccf2095f0b9807981e6fc135824
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2023-03-24 15:27:03 +0200

    24032023

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
-# APPLICATION FOR FITNESS APPLICATION
-# ===============================================
+# MAIN WINDOW FOR FITNESS APPLICATION
+# ===================================
 
 # LIBRARIES AND MODULES
-# ---------------------
-
-import sys  # For accessing system parameters
-import os  # For file path handling
-from PyQt6 import QtCore
-from PyQt6 import QtWidgets  # UI elements functionality
-from PyQt6.uic import loadUi # For loading the UI file
-# from qt_material import QtStyleTools, apply_stylesheet  # For theme adjustments
-
+import sys
+from PyQt6 import QtCore # Core functionality of Qt
+from PyQt6 import QtWidgets # UI elements functionality
+from PyQt6.uic.load_ui import loadUi
 
 # Class for the main window
 class MainWindow(QtWidgets.QMainWindow):
-    """MainWindow for the fitness app."""
-    
-    def __init__(self):
-        super().__init__()    
-    
+
+    """MainWindow for the fitness app"""
+
     # Constructor for the main window
-        loadUi('main.ui', self)
-        self.setWindowTitle('Fitness')
+    def __init__(self):
+        super().__init__()
 
         # Load the UI file
+        self.main = loadUi('main.ui', self)
 
-           # Define UI Controls ie buttons and input fields
-        self.calculatePushButton = self.calculatePushButton
-        self.calculatePushButton.clicked.connect(self.calculateAll)
+        # Define UI Controls ie buttons and input fields
+        self.calculatePB = self.calculatePushButton
+        self.calculatePB.clicked.connect(self.calculateAll)
 
-    # Define slots ie methods 
+    # Define slots ie methods
     def calculateAll(self):
-        self.bmiLabel.setValue('100') 
+        self.bmiLabel.setValue('100')
 
 if __name__ == "__main__":
     # Create the application
-
-    # Create the mainwindow(and show it)
-
-    # Start the application
-
- # Create the application
     app = QtWidgets.QApplication(sys.argv)
 
     # Create the Main Window object from MainWindow class and show it on the screen
     appWindow = MainWindow()
     appWindow.main.show()
     sys.exit(app.exec())
+    
